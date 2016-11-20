ff8ccc43f6d54a9377c65b88374d98d13a22c89c
Nogami Takaya <nogamitakaya@gmail.com>
2016-11-20 22:57:08 +0900

    テストフレームワーク導入

--- pyramid.cpp
+++ pyramid.cpp
@@ -108,8 +108,15 @@ void usage()
 }
 
 /****************************************************************************/
-int main()
+int test();
+int main(int argc, char *argv[])
 {
+#ifdef TEST
+    if(argc>=2 && strcmp(argv[1],"--test")==0 ){
+        return test();
+    }
+#endif
+
     Board board;
 
     board.init();
@@ -117,6 +124,69 @@ int main()
     return 0;
 }
 /****************************************************************************/
-void test()
-{
+#ifdef TEST
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class FunctionTest : public CPPUNIT_NS::TestFixture{
+    //テストクラス
+    CPPUNIT_TEST_SUITE(FunctionTest);//登録のスタート
+    CPPUNIT_TEST(test_test);
+    CPPUNIT_TEST_SUITE_END();//登録の終了
+
+protected:
+    Board *pBoard;
+    void test_test();
+
+public:
+    void setUp();
+    void testDown();
+};
+
+/****************************************************************************/
+CPPUNIT_TEST_SUITE_REGISTRATION(FunctionTest);
+
+//テスト起動時に実行
+void FunctionTest::setUp(){
+    pBoard = new Board();
 }
+
+//テスト終了時に実行
+void FunctionTest::testDown(){
+    delete pBoard;
+}
+/****************************************************************************/
+void FunctionTest::test_test()
+{
+
+}
+/****************************************************************************/
+int test()
+{
+    CPPUNIT_NS::TestResult controller;
+
+    //結果収集
+    CPPUNIT_NS::TestResultCollector result;
+    controller.addListener(&result);
+
+    //途中結果の収集
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    controller.addListener(&progress);
+
+    //テストを走らせる。テストを入れて走る
+    CPPUNIT_NS::TestRunner runner;
+    runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+    runner.run(controller);
+
+    //結果を標準出力にする。
+    CPPUNIT_NS::CompilerOutputter outputter(&result,CPPUNIT_NS::stdCOut());
+    outputter.write();
+
+    return result.wasSuccessful() ? 0 : 1;
+}
+#endif //TEST
